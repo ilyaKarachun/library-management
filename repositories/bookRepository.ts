3acdf09626d070d6dd7489b386af3905e0692501
Belén Robledo <beeelrobledo@gmail.com>
2023-05-16 17:33:58 -0300

    replace book_id with ISBN

--- repositories/bookRepository.ts
+++ repositories/bookRepository.ts
@@ -2,9 +2,10 @@ import BookDTO from '../dtos/bookDTO';
 import query from '../db/db'; 
 
 export class BookRepository{
+    private table: string = "books";
 
     async create(newBook: BookDTO): Promise<void>{
-        const queryText = "INSERT INTO book (isbn, title, author, year, isavailable) VALUES ($1, $2, $3, $4, $5) RETURNING id_book;";
+        const queryText = `INSERT INTO ${this.table} (ISBN, title, author, year, isavailable) VALUES ($1, $2, $3, $4, $5);`;
         const values = [newBook.ISBN, newBook.title, newBook.author, newBook.year, newBook.isAvailable];
 
         try{
@@ -15,12 +16,12 @@ export class BookRepository{
     }
 
     async getall(){
-        const queryText = "SELECT id_book, isbn, title, author, year, isavailable FROM book ORDER BY id_book;";
+        const queryText = `SELECT ISBN, title, author, year, isavailable FROM ${this.table} ORDER BY ISBN;`;
         try{
             const result = await query(queryText);
 
             return result.rows.map(
-                (row) => new BookDTO(row.id_book, row.isbn, row.title, row.author, row.year, row.isavailable)
+                (row) => new BookDTO(row.isbn, row.title, row.author, row.year, row.isavailable)
             );
 
         }catch(error){
@@ -28,25 +29,8 @@ export class BookRepository{
         }
     }
 
-    async getById(id: number) {
-        const queryText = `SELECT id_book, isbn, title, author, year, isavailable FROM book WHERE id_book = $1;`;
-        const values = [id];
-
-        try {
-          const result = await query(queryText, values);
-          if (result.rows.length === 0) {
-            return null; // Book not found
-          }
-      
-          const row = result.rows[0];
-          return new BookDTO(row.id_book, row.isbn, row.title, row.author, row.year, row.isavailable);
-        } catch (error) {
-          throw new Error(`Error while trying to get book by id: ${id}`);
-        }
-    }
-
     async getByISBN(ISBN: string) {
-        const queryText = `SELECT id_book, isbn, title, author, year, isavailable FROM book WHERE isbn = $1;`;
+        const queryText = `SELECT ISBN, title, author, year, isavailable FROM ${this.table} WHERE ISBN = $1;`;
         const values = [ISBN];
 
         try {
@@ -56,14 +40,14 @@ export class BookRepository{
           }
       
           const row = result.rows[0];
-          return new BookDTO(row.id_book, row.isbn, row.title, row.author, row.year, row.isavailable);
+          return new BookDTO(row.isbn, row.title, row.author, row.year, row.isavailable);
         } catch (error) {
           throw new Error(`Error while trying to get book by ISBN: ${ISBN}`);
         }
     }
     
     async getByTitle(title: string): Promise<BookDTO | null> {
-        const queryText = 'SELECT id_book, isbn, title, author, year, isavailable FROM book WHERE title = $1;';
+        const queryText = `SELECT ISBN, title, author, year, isavailable FROM ${this.table} WHERE title = $1;`;
         const values = [title];
       
         try {
@@ -73,15 +57,15 @@ export class BookRepository{
           }
       
           const row = result.rows[0];
-          return new BookDTO(row.id_book, row.isbn, row.title, row.author, row.year, row.isavailable);
+          return new BookDTO(row.isbn, row.title, row.author, row.year, row.isavailable);
         } catch (error) {
           throw new Error(`Error while trying to get book by title: ${title}`);
         }
     }
     
     async update(newBook: BookDTO) {
-        const queryText = `UPDATE book SET ISBN = $2, title = $3, author = $4, year = $5, isAvailable = $6 WHERE id_book = $1`;
-        const values = [newBook.idBook, newBook.ISBN, newBook.title, newBook.author, newBook.year, newBook.isAvailable];
+        const queryText = `UPDATE ${this.table} SET ISBN = $1, title = $2, author = $3, year = $4, isAvailable = $5 WHERE ISBN = $1`;
+        const values = [newBook.ISBN, newBook.title, newBook.author, newBook.year, newBook.isAvailable];
 
         try {
           await query(queryText, values);
@@ -90,14 +74,14 @@ export class BookRepository{
         }
     }
     
-    async delete(id: number) {
-        const queryText = `DELETE FROM book WHERE id_book = $1;`;
-        const values = [id];
+    async delete(ISBN: string) {
+        const queryText = `DELETE FROM ${this.table} WHERE ISBN = $1;`;
+        const values = [ISBN];
 
         try {
           await query(queryText, values);
         } catch (err) {
-          throw new Error(`Error while trying to delete book by id: ${id}`);
+          throw new Error(`Error while trying to delete book by ISBN: ${ISBN}`);
         }
     }
 
