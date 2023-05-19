c7f1ab187934e8a4a11951d55f7a0bf10fe8433c
Rafal-wq <rafal_wilczewski@wp.pl>
2023-05-19 15:59:25 +0200

    Init books dto

---
+++ dto/booksDto.ts
@@ -0,0 +1,15 @@
+export class BooksRecord {
+    title: string;
+    author: string;
+    publicationYear: number;
+    isbn: number;
+    isAvailable: boolean;
+
+    constructor(title: string, author: string, publicationYear: number, isbn: number, isAvailable: boolean) {
+        this.author = author;
+        this.title = title;
+        this.publicationYear = publicationYear;
+        this.isbn = isbn;
+        this.isAvailable = isAvailable;
+    }
+}
