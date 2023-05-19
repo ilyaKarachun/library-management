28fd60c6411028cadfd110d2466f4e55f414e074
Вадим Бабаев <0463503@gmail.com>
2023-05-19 16:22:00 +0200

    change dbconnection.ts

--- src/db/dbConnection.ts
+++ src/db/dbConnection.ts
@@ -2,9 +2,9 @@ import pkg from "pg";
 const { Pool } = pkg;
 
 export const pool = new Pool({
-  user: process.env.PG_USER || "postgres",
-  host: process.env.PGHOST || "db",
-  database: process.env.PG_DATABASE || "library_managment",
-  password: process.env.PG_PASSWORD || "root",
+  user: process.env.PG_USER!,
+  host: process.env.PG_HOST!,
+  database: process.env.PG_DATABASE!,
+  password: process.env.PG_PASSWORD!,
   port: 5432,
 });
