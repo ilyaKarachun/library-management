becaca84de61c8ca509a2b806575c494681bf672
juan pablo bustamante <bustamante.jpm@gmail.com>
2023-05-22 10:05:23 -0300

    add routes

--- repositories/borrowingRepository.ts
+++ repositories/borrowingRepository.ts
@@ -28,16 +28,6 @@ export class BorrowingRepository{
             throw new Error(`Error while checking availability`);
         }
     };
-    async setAvailability(ISBN: string, isAvailable: boolean){
-        const queryText = 'UPDATE books SET is_available = $1 WHERE isbn = $2';
-        const values = [isAvailable, ISBN];
-        try{
-            await query(queryText, values); 
-        }
-        catch(err){
-            throw new Error(`Error while setting availability: ${err.message}`);
-        }
-    };
     async getBorrowingHistory(id: number){
         const queryText = 'SELECT bb.isbn, bb.borrowing_date, bb.due_date, returned_date FROM books_borrowers WERE borrower_id = $1';
         const values = [id];
--- routers/borrowingRouter.ts
+++ routers/borrowingRouter.ts
@@ -6,4 +6,7 @@ const router = Router();
 const borrowingController = new BorrowingController();
 
 router.post('/borrowings', authenticate, borrowingController.borrow);
+router.get('/borrowings/history', authenticate, borrowingController.getBorrowingHistory);
+router.get('/borrowings/overdue', authenticate, borrowingController.borrowerDueDates);
+router.put('/borrowings/availability', authenticate, borrowingController.checkAvailability);
 export default router;
