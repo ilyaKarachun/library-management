16ee538b8b0345fbd041709e10b331691b4d2d93
Belén Robledo <beeelrobledo@gmail.com>
2023-05-23 13:04:26 -0300

    fix problem in book router

--- src/routers/bookRouter.ts
+++ src/routers/bookRouter.ts
@@ -7,7 +7,7 @@ const router = express.Router();
 
 router.get("/books", authenticate, booksController.getBooks);
 router.get("/books/:isbn", authenticate, booksController.getBookByISBN);
-router.get("/books/title/:title", authenticate, booksController.getBookByISBN);
+router.get("/books/title/:title", authenticate, booksController.getBookByTitle);
 router.post("/books", authenticate, booksController.postBook);
 router.put("/books", authenticate, booksController.putBook);
 router.delete("/books/:isbn", authenticate, booksController.deleteBook);
