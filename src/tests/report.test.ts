70c94eb8c3c2c0569710c919de90f4accd2b6977
Duke0404 <01165826@pw.edu.pl>
2023-05-24 18:10:19 +0200

    Updates

--- src/tests/report.test.ts
+++ src/tests/report.test.ts
@@ -9,3 +9,21 @@ beforeAll(async () => {
 		.send(adminCredentials)
 	cookie = response.headers["set-cookie"]
 })
+
+describe("GET /reports/books", () => {
+	const url = "/reports/books"
+
+	it("should return 401 if user is not logged in", async () =>
+		supertest(baseUrl).get(url).expect(401))
+
+	it("should return 200", async () =>
+	{
+		supertest(baseUrl).post("/books").set("Cookie", cookie).send({ ISBN: 9999, title: "Test Book 1", author: "Me", year: 2023 })
+		supertest(baseUrl).post("/books").set("Cookie", cookie).send({ ISBN: 9998, title: "Test Book 2", author: "Me", year: 2023 })
+
+		supertest(baseUrl).get(url).set("Cookie", cookie).expect(200)
+
+		supertest(baseUrl).delete("/books/9999").set("Cookie", cookie)
+		supertest(baseUrl).delete("/books/9998").set("Cookie", cookie)
+	})
+})
