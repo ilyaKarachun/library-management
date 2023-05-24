a8b1ccf9a0e589819e51a22d55d937294520662a
Duke0404 <01165826@pw.edu.pl>
2023-05-24 04:25:21 +0200

    Added functionality

--- src/repositories/borrowingRepository.ts
+++ src/repositories/borrowingRepository.ts
@@ -1,73 +1,118 @@
 import { pool } from "../db/dbConnection";
 import { BorrowingDTO } from '../dtos/borrowingDTO';
 
-export class BorrowingRepository{
-    async borrow(borrowerId: number, dueDate: string, ISBNs: Array<string>): Promise<void>{
+export class BorrowingRepository {
+	async borrow(ISBN: string, borrowerId: string, dueDate: Date) {
+		const client = await pool.connect()
 
-    };
+		try {
+			await client.query("BEGIN")
+			await client.query(
+				"INSERT INTO books_borrowers (isbn, borrower_id, borrowing_date, due_date) VALUES ($1, $2, $3, $4)",
+				[ISBN, borrowerId, new Date(), dueDate]
+			)
+			await client.query(
+				"UPDATE books SET is_available = false WHERE isbn = $1",
+				[ISBN]
+			)
+			await client.query("COMMIT")
+		} catch (err) {
+			await client.query("ROLLBACK")
+			throw new Error("Error while borrowing")
+		} finally {
+			client.release()
+		}
+	}
 
-    async return(ISBN: string) {
-    
-    };
+	async return(ISBN: string) {
+		const client = await pool.connect()
 
-    async checkAvailability(ISBN: string){
-        const client = await pool.connect();
-        const queryText = 'SELECT is_available FROM books WHERE isbn = $1';
-        const values = [ISBN];
-        try{
-            const result = await client.query(queryText, values);
-            if(result.rows.length > 0){
-                const {is_available} = result.rows[0];
-                return is_available;
-            }
-        }
-        catch(err){
-            throw new Error(`Error while checking availability`);
-        }
-        finally{
-            client.release();
-        }
-    };
+		try {
+			await client.query("BEGIN")
+			await client.query(
+				"UPDATE books_borrowers SET returned_date = $1 WHERE isbn = $2",
+				[new Date(), ISBN]
+			)
+			await client.query(
+				"UPDATE books SET is_available = true WHERE isbn = $1",
+				[ISBN]
+			)
+			await client.query("COMMIT")
+		} catch (err) {
+			await client.query("ROLLBACK")
+			throw new Error("Error while returning")
+		} finally {
+			client.release()
+		}
+	}
 
-    async getBorrowingHistory(id: number){
-        const client = await pool.connect();
-        const queryText = 'SELECT borrowing_id, isbn, borrowing_date, due_date, returned_date FROM books_borrowers WHERE borrower_id = $1';
-        const values = [id];
-        try{
-            const result = await client.query(queryText, values);
-            const history: BorrowingDTO[] = []
-            result.rows.forEach((row) => {
-                const borrowing = new BorrowingDTO(row.borrowing_id, row.isbn, id, row.borrowing_date, row.due_date, row.returned_date);
-                history.push(borrowing);
-            });
-            return history;
-        }
-        catch(err){
-            throw new Error(`Error while getting borrower history`);
-        }
-        finally{
-            client.release();
-        }
-    };
+	async checkAvailability(ISBN: string) {
+		const client = await pool.connect()
+		const queryText = "SELECT is_available FROM books WHERE isbn = $1"
+		const values = [ISBN]
+		try {
+			const result = await client.query(queryText, values)
+			if (result.rows.length > 0) {
+				const { is_available } = result.rows[0]
+				return is_available
+			}
+		} catch (err) {
+			throw new Error(`Error while checking availability`)
+		} finally {
+			client.release()
+		}
+	}
 
-    async getBorrowerDueDates(id: number){
-        const client = await pool.connect();
-        const queryText = 'SELECT borrowing_id, isbn, borrowing_date, due_date FROM books_borrowers WHERE borrower_id = $1 AND returned_date IS NULL';
-        const values = [id];
-        try{
-            const result = await client.query(queryText, values);
-            const dueDates: BorrowingDTO[] = []
-            result.rows.forEach((row) => {
-                const borrowing = new BorrowingDTO(row.borrowing_id, row.isbn, id, row.borrowing_date, row.due_date);
-                dueDates.push(borrowing);
-            });
-            return dueDates;
-        }
-        catch(err){
-            throw new Error(`Error while getting borrower due dates`);
-        }
-        finally{
-            client.release();
-        }
-    };
+	async getBorrowingHistory(id: number) {
+		const client = await pool.connect()
+		const queryText =
+			"SELECT borrowing_id, isbn, borrowing_date, due_date, returned_date FROM books_borrowers WHERE borrower_id = $1"
+		const values = [id]
+		try {
+			const result = await client.query(queryText, values)
+			const history: BorrowingDTO[] = []
+			result.rows.forEach((row) => {
+				const borrowing = new BorrowingDTO(
+					row.borrowing_id,
+					row.isbn,
+					id,
+					row.borrowing_date,
+					row.due_date,
+					row.returned_date
+				)
+				history.push(borrowing)
+			})
+			return history
+		} catch (err) {
+			throw new Error(`Error while getting borrower history`)
+		} finally {
+			client.release()
+		}
+	}
+
+	async getBorrowerDueDates(id: number) {
+		const client = await pool.connect()
+		const queryText =
+			"SELECT borrowing_id, isbn, borrowing_date, due_date FROM books_borrowers WHERE borrower_id = $1 AND returned_date IS NULL"
+		const values = [id]
+		try {
+			const result = await client.query(queryText, values)
+			const dueDates: BorrowingDTO[] = []
+			result.rows.forEach((row) => {
+				const borrowing = new BorrowingDTO(
+					row.borrowing_id,
+					row.isbn,
+					id,
+					row.borrowing_date,
+					row.due_date
+				)
+				dueDates.push(borrowing)
+			})
+			return dueDates
+		} catch (err) {
+			throw new Error(`Error while getting borrower due dates`)
+		} finally {
+			client.release()
+		}
+	}
 }
